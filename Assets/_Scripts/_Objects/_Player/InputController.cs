02256f87cdc7706d0bab68a2fe5dbf69a4e6fd21
Taylor Libonati <Tshadow05@gmail.com>
2014-09-12 20:10:28 -0700

    started working on input

--- Assets/_Scripts/_Objects/_Player/InputController.cs
+++ Assets/_Scripts/_Objects/_Player/InputController.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class InputController : MonoBehaviour {
 	private PlayerController player;
 	private float xMovement = 0;
 	private float yMovement = 0;
+	private Dictionary<string,int> key;
+	private custom_inputs input;
 
 	public string idName = "Player1";
+	public int joystickNumber = 0;
 	[HideInInspector]
 	public bool lockMovement = false;
 
@@ -15,16 +19,34 @@ public class InputController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		player = GetComponent<PlayerController>();
-	}
 
+	}
+	void Awake(){
+		input = GetComponent<custom_inputs> ();
+		key = new Dictionary<string,int> ();
+		key.Add ("Start",0);
+		key.Add ("Jump",1);
+		key.Add ("Block",2);
+		key.Add ("Dodge",3);
+		key.Add ("Melee",4);
+		key.Add ("Ranged",5);
+	}
+	public bool keyDown(string name){
+		return input.isInputDown [key [name]];
+	}
+	public bool keyUp(string name){
+		return input.isInputUp [key [name]];
+	}
 	// Update is called once per frame
 	void Update () {
+		//xMovement = Input.GetAxis(idName + "Horizontal");
+		//yMovement = Input.GetAxis(idName + "Vertical");
 		xMovement = Input.GetAxis(idName + "Horizontal");
 		yMovement = Input.GetAxis(idName + "Vertical");
 		dir = new Vector3 (xMovement, yMovement, 0).normalized;
 		if(!lockMovement){
 			player.moveDir(xMovement);
-			if(Input.GetButton(idName + "Jump")){
+			if(keyDown("Jump")){
 				//if(yMovement<0){
 				//	player.dropBelow();
 				//}else{
@@ -34,25 +56,35 @@ public class InputController : MonoBehaviour {
 				player.endJump();
 			} 
 			//Shielding
-			if(Input.GetButtonDown(idName + "Shield")){
+			if(keyDown("Shield")){
 				player.shield();
 			}
 			//Dodgeing
-			if(Input.GetButtonDown(idName + "Dodge")){
+			if(keyDown(idName + "Dodge")){
 				player.dodge();
 			}
 		}
 		/*if(Input.GetButtonUp(idName + "Shield")){
 			player.stopShield();
 		}*/
-		int numberOfAttackButtons = 4;
+		if(keyDown("Melee")){
+			player.attackPress(0);
+		}else if(keyUp("Melee")){
+			player.attackRelease(0);
+		}
+		if(keyDown("Ranged")){
+			player.attackPress(1);
+		}else if(keyUp("Ranged")){
+			player.attackRelease(1);
+		}
+		/*int numberOfAttackButtons = 4;
 		for(int i=0; i<numberOfAttackButtons; i++){
-			if(Input.GetButtonDown(idName + "Attack"+i)){
+			if(keyDown("Attack"+i)){
 				player.attackPress(i);
 			}else if(Input.GetButtonUp(idName + "Attack"+i)){
 				player.attackRelease(i);
 			}
-		}
+		}*/
 		checkDirection();
 	}
 	private void checkDirection(){
